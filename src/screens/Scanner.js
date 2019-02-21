59dbbc178dd114c110376ea456cd2c4172d11506
Suyesh Bhandari <suyeshb@gmail.com>
2019-02-21 18:50:15 -0500

    .

--- src/screens/Scanner.js
+++ src/screens/Scanner.js
@@ -1,8 +1,39 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import { BarCodeScanner, Permissions } from "expo";
 
-export default () => (
-  <View>
-    <Text>Scanner</Text>
-  </View>
-);
+class Scanner extends React.Component {
+  state = {
+    hasCameraPermission: null
+  };
+
+  handleBarCodeScanned = e => {
+    const { type, data } = e;
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  };
+
+  componentDidMount = async () => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasCameraPermission: status === "granted" });
+  };
+
+  render() {
+    const { hasCameraPermission } = this.state;
+
+    if (hasCameraPermission === null) {
+      return <Text>Requesting for camera permission</Text>;
+    }
+
+    if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    }
+    return (
+      <BarCodeScanner
+        onBarCodeScanned={this.handleBarCodeScanned}
+        style={StyleSheet.absoluteFill}
+      />
+    );
+  }
+}
+
+export default Scanner;
